935573236d40669a13d74ce081fae2645740a695
sara khalil <sarakhalil3377@gmail.com>
2022-08-27 10:20:08 +0300

    Completed assignment

--- main.js
+++ main.js
@@ -4,6 +4,31 @@ const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
 
+const likes = document.getElementsByClassName("like");
+const error = document.getElementById("modal");
+console.log(likes);
+
+for (const like of likes) {
+  like.addEventListener("click", () => {
+    mimicServerCall()
+      .then(res => {
+        const icon = like.childNodes[1];
+        if (icon.textContent == FULL_HEART) {
+          icon.textContent = EMPTY_HEART;
+        }
+        else {
+          icon.textContent = FULL_HEART;
+        }
+        icon.classList.toggle("activated-heart");
+      })
+      .catch(() => {
+        error.classList.remove("hidden")
+        setTimeout(function () {
+          error.classList.add("hidden");
+        }, 3000);
+      });
+  });
+}
 
 
 
@@ -11,9 +36,9 @@ const FULL_HEART = '♥'
 // Don't change the code below: this function mocks the server response
 //------------------------------------------------------------------------------
 
-function mimicServerCall(url="http://mimicServer.example.com", config={}) {
-  return new Promise(function(resolve, reject) {
-    setTimeout(function() {
+function mimicServerCall(url = "http://mimicServer.example.com", config = {}) {
+  return new Promise(function (resolve, reject) {
+    setTimeout(function () {
       let isRandomFailure = Math.random() < .2
       if (isRandomFailure) {
         reject("Random server error. Try again.");
